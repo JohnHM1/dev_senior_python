55a87cebce5ed1b152639bd6e6d8ef08f190fc2b
JohnHM1 <91163544+JohnHM1@users.noreply.github.com>
2024-10-26 03:09:50 +0200

    Ejercicio Clase 2

---
+++ clase2.py
@@ -0,0 +1,21 @@
+#Ejercicio 1
+variableUsuario= (input("Digite el primer un numero: "))
+numero1 = float(variableUsuario)
+variableUsuario=(input("Digite el segundo un numero: "))
+numero2 = float(variableUsuario)
+
+
+isNumeric= variableUsuario.isnumeric()
+if not isNumeric:
+    print("El numero no es un Digito.")
+
+suma = numero1 + numero2
+resta = numero1 - numero2
+multiplicacion = numero1 * numero2
+division = numero1 / numero2
+
+
+print(f"Suma: {suma}")
+print(f"Resta: {resta}")
+print(f"Multiplicacion: {multiplicacion}")
+print(f"Division: {division}")
